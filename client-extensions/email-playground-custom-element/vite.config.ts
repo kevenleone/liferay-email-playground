969bdf910db3147cbd0ce06bc71e0893ea68ac75
Keven Leone <kevenleone.me@gmail.com>
2026-04-20 09:40:02 -0300

    Upgrade dependencies and use Vite8

--- client-extensions/email-playground-custom-element/vite.config.ts
+++ client-extensions/email-playground-custom-element/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
@@ -38,6 +38,5 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
-        splitVendorChunkPlugin(),
     ],
 });
